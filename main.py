28435cd9d39f4b282185d82f622cdd558bb1d20a
Vladimir <veliseykin2000@gmail.com>
2022-08-08 22:26:37 -0700

    finish

---
+++ main.py
@@ -0,0 +1,54 @@
+# --exersise 1
+# -- answer 2
+# SELECT *
+# FROM actor
+# WHERE last_name LIKE 'W%';
+#
+# --exersise 2
+# --answer 5607
+# SELECT *
+# FROM payment
+# WHERE amount >= 3.99 AND amount <= 5.99;
+#
+# --exersise 3
+# --answer 32,33,34,35,36
+# SELECT inventory_id, film_id, store_id
+# FROM inventory;
+#
+# --exersise 4
+# --answer 2
+# SELECT *
+# FROM customer
+# WHERE first_name LIKE 'Wil%'
+#
+# --exersise 5
+# --answer 2
+# SELECT rental_id, rental_date, inventory_id, staff_id
+# FROM rental;
+#
+# --exersise 6
+# --answer 2
+# SELECT first_name
+# FROM staff;
+#
+# --exersise 7
+# --answer Wrong Behavior(id 993)
+# SELECT film_id, actor_id
+# FROM film_actor;
+#
+#
+# --exersise 8
+# --answer 21
+# SELECT store_id, last_name
+# FROM customer
+# WHERE last_name LIKE '%es';
+#
+# --exersise 9
+# --answer
+# SELECT *
+# FROM rental
+# GROUP BY customer_id
+# HAVING;
+
+
+
